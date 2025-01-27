d594fb90eff4c555fa592771f9e970e9e2dda16b
ZY Deng <me@zydeng.com>
2025-01-27 17:27:51 +0800

    move stamp check function into separate file

---
+++ demo/backend/validator.js
@@ -0,0 +1,33 @@
+import { createHash } from 'crypto'
+
+export function checkStamp(stamp, resource, difficulty, expiration) {
+  const [ver, bits, date, res, ext, rand, counter, _] = stamp.split(":")
+  if (ver !== "1") {
+    return "only hashcash v1 is supported"
+  }
+  if (!counter || _ !== undefined) {
+    return "malformed stamp"
+  }
+  if (expiry(date, expiration)) {
+    return "expired stamp"
+  }
+  if (parseInt(bits) < difficulty || resource !== res || !hash32(stamp, difficulty)) {
+    return "invalid stamp"
+  }
+  return "ok"
+}
+
+function hash32(stamp, bits) {
+  const sha1sum = createHash('sha1')
+  sha1sum.update(stamp)
+  const hashed = sha1sum.digest('hex')
+  const hashed32 = parseInt(hashed.slice(0, 8), 16)
+  return Math.clz32(hashed32) >= bits
+}
+
+function expiry(date, expiration) {
+  const now = Date.now()
+  const datestr = `20${date.slice(0,2)}/${date.slice(2,4)}/${date.slice(4,6)}`
+  const d = Date.parse(datestr)
+  return isNaN(d) || d > now || d < now - expiration
+}
