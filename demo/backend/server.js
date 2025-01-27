1a2879283ddc3b95c7d5fceeebb522f69342b7a3
ZY Deng <me@zydeng.com>
2025-01-27 17:35:27 +0800

    enable cors preflight for chrome

--- demo/backend/server.js
+++ demo/backend/server.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 import { checkStamp } from './validator.js'
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.post('/send', (req, res) => {
+app.options('/send', cors())
+app.post('/send', cors(), (req, res) => {
   const stamp = req.body.pow
   const email = req.body.email
   res.send(checkStamp(stamp, email, 20, 2*24*3600*1000))
